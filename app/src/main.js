c526f7c6b7acabf8dd2af4b45ba5620488e6c381
cyph404 <nicoleblanchette05@gmail.com>
2024-01-19 23:18:11 -0500

    please work

--- app/src/main.js
+++ app/src/main.js
@@ -78,10 +78,8 @@ ball.style.top = `${(maxX * x) / 180 - 10}px`;// rotating device around the x ax
   // text.textContent = 'i did it!'
   // text.style.color = 'yellow'
 
-  const xWalk = (x / width * walk) - (walk / 2);
-  const yWalk = (y / height * walk) - (walk / 2);
-  text.style.textShadow = `${y * 2 / 180 - 10}px ${x * 2 / 180 - 10}px 0 chartreuse,
-  ${(y / window.height * walk) - (walk / 2)}px ${(x / window.width * walk) - (walk / 2)}px 0 red`
+
+  text.style.textShadow = `${y - 90}px ${x -90}px 0 chartreuse,`
    
 };
 
